89eb5d089b5ca2f3bbb5709126d8f3956cfb4bd3
maru_macbook <69277660@naver.com>
2024-10-28 12:49:26 +0900

    불필요한 내용 제거

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730087029',resources: [
+    cacheName: 'chirpy-1730087323',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
