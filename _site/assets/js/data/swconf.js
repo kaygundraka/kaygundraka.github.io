d6bee17b9f799b55f9cf44362b94d1af1b8db00f
maru_macbook <69277660@naver.com>
2024-10-28 13:12:50 +0900

    아바타 안나오는것 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730087323',resources: [
+    cacheName: 'chirpy-1730088737',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
